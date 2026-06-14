87c17636063918592f919d4d9eea0f002479431c
Jason Thrasher <jasonthrasher+macbook-pro-m4-128gb-2024@gmail.com>
2026-06-14 19:21:14 +0200

    added downloads, and ignore file

--- downloads/download.sh
+++ downloads/download.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 curl -O https://www.mkgmap.org.uk/download/splitter-r654.zip
-curl -O https://www.mkgmap.org.uk/download/mkgmap-r4924.zip
+unzip -f splitter-r654.zip
 
+curl -O https://www.mkgmap.org.uk/download/mkgmap-r4924.zip
+unzip -f mkgmap-r4924.zip
+
+curl -O http://m.m.i24.cc/osmconvert.c
+gcc osmconvert.c -lz -O3 -o osmconvert
